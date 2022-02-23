cbeef10a45ec4169d122471c6d5c2a1a89a6b645
Sarang Wadode <sarangwadode@gmail.com>
2022-02-23 23:47:08 +0530

    random quote and author generated

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 export default function Homepage() {
   const [quotesData, setquotesData] = useState([]);
+  const [quote, setquote] = useState('To share our stories is not only a worthwhile endeavor for the storyteller, but for those who hear our stories and feel less alone because of it.');
+  const [author, setauthor] = useState('Joyce Maynard');
 
   useEffect(() => {
     const fetchData = async () => {
       console.log('fetching data...');
-      const response = await fetch('https://quote-garden.herokuapp.com/api/v3/quotes?limit=1000')
+      const response = await fetch('https://quote-garden.herokuapp.com/api/v3/quotes?limit=2000')
       const quotes = await response.json()
+      console.log(quotes.data);
       setquotesData(quotes.data)
     }
     fetchData()
   }, [])
 
+  const handleClick = () => {
+    const random = Math.floor(Math.random() * quotesData.length)
+    setquote(quotesData[random].quoteText)
+    setauthor(quotesData[random].quoteAuthor)
+  }
+
 
   return (
     <div>
       <h1>Homepage</h1>
-      <div>{JSON.stringify(quotesData)}</div>
+      <div>{quote}</div>
+      <div>{author}</div>
+      <button onClick={handleClick}>Click me</button>
     </div>
     
   )
